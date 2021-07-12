b86966eb7b016f9184f7eb5ec05345165bfe886d
webPanyulin <2841113791@qq.com>
2021-07-12 17:26:17 +0800

    📦 卡片搬运

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,14 +1,15 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:29:03
- * @LastEditTime: 2021-07-12 16:25:46
+ * @LastEditTime: 2021-07-12 17:21:07
  * @Description: 
- * @LastEditors: Mr.wang
+ * @LastEditors: Pan.Yu.Lin
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
 -->
 <template>
   <cal-icon type="collapsed" />
-  <cal-button>asdadaS</cal-button>
+  <!-- <cal-button>asdadaS</cal-button> -->
+  <cal-card>1212</cal-card>
 </template>
 
 <script lang="ts" setup>
