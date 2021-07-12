0dfcaf3774b0aa43c49fdcab10dac30e6236d336
燃芯 <56920162+RanxinStart@users.noreply.github.com>
2021-07-12 17:57:38 +0800

    布局组件

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:29:03
- * @LastEditTime: 2021-07-12 17:04:39
+ * @LastEditTime: 2021-07-12 17:55:48
  * @Description: 
  * @LastEditors: Zhilong
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -9,10 +9,17 @@
 <template>
   <cal-icon type="collapsed" />
   <cal-button>asdadaS</cal-button>
-  <cal-grid :size="{ width: 50, height: 60 }">
-    <div> 0 </div>
+  <cal-grid :size="{ width: 300, height: 300 }">
+    <div>
+      <div>??</div>
+    </div>
     <div> 1 </div>
     <div> 2 </div>
+    <div> 3 </div>
+    <div> 4 </div>
+    <div> 5 </div>
+    <div> 5 </div>
+    <div> 5 </div>
   </cal-grid>
 </template>
 
@@ -20,4 +27,8 @@
   const props = defineProps({})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  div {
+    text-align: center;
+  }
+</style>
