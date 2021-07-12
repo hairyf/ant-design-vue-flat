5293d5de550d9a9028b9d973dc374e59512bb276
13169588842 <2531161998@qq.com>
2021-07-12 18:35:54 +0800

    搬组件

--- package/components.ts
+++ package/components.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:12:41
- * @LastEditTime: 2021-07-12 17:04:49
+ * @LastEditTime: 2021-07-12 18:33:31
  * @Description:
- * @LastEditors: Zhilong
+ * @LastEditors: Mr.wang
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
 export { default as CalButton } from './components/button/src/Button.vue'
@@ -23,3 +23,5 @@ export { default as CalIcon } from './components/icon/src/Icon.vue'
 export { default as CalImage } from './components/image/src/Image.vue'
 export { default as CalGrid } from './components/grid/src/Grid.vue'
 export { default as CalImageVerif } from './components/image-verif/src/ImageVerif.vue'
+export { default as CalPageSelect } from './components/page-select/src/PageSelect.vue'
+export { default as CalPagination } from './components/pagination/src/Pagination.vue'
