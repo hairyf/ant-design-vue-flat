02e633168572acbefe160f16282b0caeb7777eb1
13169588842 <2531161998@qq.com>
2021-07-12 15:24:09 +0800

    input

--- package/components.ts
+++ package/components.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:12:41
- * @LastEditTime: 2021-07-09 19:59:33
+ * @LastEditTime: 2021-07-12 14:59:50
  * @Description:
- * @LastEditors: Mr.Mao
+ * @LastEditors: Mr.wang
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
 export { default as CalButton } from './components/button/src/Button.vue'
@@ -14,3 +14,6 @@ export { default as CalLayout } from './components/layout/src/Layout.vue'
 export { default as CalLayoutContent } from './components/layout/src/LayoutContent.vue'
 export { default as CalLayoutSider } from './components/layout/src/LayoutSider.vue'
 export { default as CalBackground } from './components/background/src/Background.vue'
+export { default as CalInputPassword } from './components/input/src/inputPassword.vue'
+export { default as CalInputDate } from './components/input/src/inputDate.vue'
+export { default as CalInputNumber } from './components/input/src/inputNumber.vue'
