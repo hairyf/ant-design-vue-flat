f0f2981d52d14736a883aa96fdad888856f28934
TuiMao233 <wwu710632@gmail.com>
2021-07-13 17:53:30 +0800

    🐛 修复 bug 异常。

--- package/components/config-provider/src/ConfigProvider.vue
+++ package/components/config-provider/src/ConfigProvider.vue
@@ -1,16 +1,14 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-05-22 14:18:13
- * @LastEditTime: 2021-07-09 16:20:03
+ * @LastEditTime: 2021-07-13 17:53:10
  * @Description: 配置注入
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
 -->
 <template>
   <div class="cal-config-provider">
-    <config-provider v-bind="props">
-      <slot></slot>
-    </config-provider>
+    <slot></slot>
   </div>
 </template>
 <script lang="ts">
--- script/rollup.config.js
+++ script/rollup.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-07 15:17:55
- * @LastEditTime: 2021-07-13 17:06:53
+ * @LastEditTime: 2021-07-13 17:15:00
  * @Description:
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
