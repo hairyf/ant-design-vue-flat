176e548b40336fb6bf9bffab1fc3259b5493c367
13169588842 <2531161998@qq.com>
2021-07-13 09:52:10 +0800

    搬移组件

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:29:03
- * @LastEditTime: 2021-07-12 20:00:14
+ * @LastEditTime: 2021-07-13 09:49:49
  * @Description: 
  * @LastEditors: Mr.wang
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -21,12 +21,18 @@
     <div> 5 </div>
     <div> 5 </div>
   </cal-grid> -->
+  <cal-tabs v-model:activeKey="currentType">
+    <cal-tab-pane tab="微信支付" key="1"> 211 </cal-tab-pane>
+    <cal-tab-pane tab="支付宝支付" key="2"> 122 </cal-tab-pane>
+    <cal-tab-pane tab="在线客服" key="3"> 311 </cal-tab-pane>
+  </cal-tabs>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue'
+  import CalTabs from '@/components/CustomAntds/CalTabs/index.vue'
   const props = defineProps({})
-  const navigationStep = ref(false)
+  const currentType = ref('1')
 </script>
 
 <style lang="scss" scoped></style>
