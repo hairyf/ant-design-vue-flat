772ee9f2a9e0c18e1e35365d71960351775ed5f4
13169588842 <2531161998@qq.com>
2021-07-13 17:14:20 +0800

    修改tree组件

--- package/components/tree/src/Tree.vue
+++ package/components/tree/src/Tree.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.wang
  * @Date: 2021-07-13 10:23:56
- * @LastEditTime: 2021-07-13 10:28:17
+ * @LastEditTime: 2021-07-13 17:14:16
  * @Description: 树状图
  * @LastEditors: Mr.wang
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -39,5 +39,16 @@
   }
   .ant-tree-checkbox-indeterminate .ant-tree-checkbox-inner::after {
     background-color: var(--common-primary-color) !important;
+  }
+  .ant-tree-treenode-checkbox-checked {
+    .ant-tree-checkbox-disabled {
+      .ant-tree-checkbox-inner {
+        border: 1px solid #d9d9d9;
+        background-color: #d9d9d9;
+      }
+      .ant-tree-checkbox-inner::after {
+        border-color: #fff;
+      }
+    }
   }
 </style>
