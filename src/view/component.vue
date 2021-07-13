cd5bb5aedd9ce9f10629900f38be5e2ee16b0804
燃芯 <56920162+RanxinStart@users.noreply.github.com>
2021-07-13 16:43:40 +0800

    完成table

--- src/view/component.vue
+++ src/view/component.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:29:03
- * @LastEditTime: 2021-07-13 12:01:46
+ * @LastEditTime: 2021-07-13 15:59:45
  * @Description: 
  * @LastEditors: Zhilong
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -9,6 +9,7 @@
 <template>
   <cal-button>asdadaS</cal-button>
   <cal-table
+    sync-space
     :list="[
       {
         a: [
@@ -20,9 +21,11 @@
       { a: [{ c: 'c', d: 'd' }], b: '起来了' }
     ]"
   >
-    <cal-table-option title="改变" type="list" index="a">
-      <cal-table-option title="我是C" index="c"> </cal-table-option>
-      <cal-table-option title="我是D"> </cal-table-option>
+    <cal-table-option index="a" type="list" max-width="250">
+      <cal-table-option title="我是C" index="c">
+        <div>xxx</div>
+      </cal-table-option>
+      <cal-table-option title="我是D" index="d"> </cal-table-option>
     </cal-table-option>
     <cal-table-option title="test" index="a"> </cal-table-option>
     <cal-table-option title="测试" index="b"> </cal-table-option>
