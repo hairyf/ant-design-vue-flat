4541ba8cc937761102d8543b21d739026706a1d0
TuiMao233 <wwu710632@gmail.com>
2021-07-14 15:49:27 +0800

    🍻 新增内容。

--- package/components/transition/src/CollapseTransition.vue
+++ package/components/transition/src/CollapseTransition.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-05-22 10:38:45
- * @LastEditTime: 2021-07-13 09:46:45
+ * @LastEditTime: 2021-07-14 15:45:15
  * @Description: 下拉收起动画组件
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -13,80 +13,75 @@
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue'
+  export default defineComponent({ name: 'CalCollapseTransition' })
+</script>
+<script lang="ts" setup>
+  const on = {
+    beforeEnter(el: HTMLDivElement) {
+      el.classList.add('collapse-transition')
+      if (!el.dataset) (el.dataset as any) = {}
 
-  export default defineComponent({
-    name: 'CalCollapseTransition',
-    setup() {
-      return {
-        on: {
-          beforeEnter(el: HTMLDivElement) {
-            el.classList.add('collapse-transition')
-            if (!el.dataset) (el.dataset as any) = {}
+      el.dataset.oldPaddingTop = el.style.paddingTop
+      el.dataset.oldPaddingBottom = el.style.paddingBottom
 
-            el.dataset.oldPaddingTop = el.style.paddingTop
-            el.dataset.oldPaddingBottom = el.style.paddingBottom
+      el.style.height = '0'
+      el.style.paddingTop = '0'
+      el.style.paddingBottom = '0'
+    },
 
-            el.style.height = '0'
-            el.style.paddingTop = '0'
-            el.style.paddingBottom = '0'
-          },
-
-          enter(el: HTMLDivElement) {
-            el.dataset.oldOverflow = el.style.overflow
-            if (el.scrollHeight !== 0) {
-              el.style.height = el.scrollHeight + 'px'
-              el.style.paddingTop = el.dataset.oldPaddingTop || '0'
-              el.style.paddingBottom = el.dataset.oldPaddingBottom || '0'
-            } else {
-              el.style.height = ''
-              el.style.paddingTop = el.dataset.oldPaddingTop || '0'
-              el.style.paddingBottom = el.dataset.oldPaddingBottom || '0'
-            }
-
-            el.style.overflow = 'hidden'
-          },
-
-          afterEnter(el: HTMLDivElement) {
-            // for safari: remove class then reset height is necessary
-            el.classList.remove('collapse-transition')
-            el.style.height = ''
-            el.style.overflow = el.dataset.oldOverflow || '0'
-          },
-
-          beforeLeave(el: HTMLDivElement) {
-            if (!el.dataset) (el.dataset as any) = {}
-            el.dataset.oldPaddingTop = el.style.paddingTop
-            el.dataset.oldPaddingBottom = el.style.paddingBottom
-            el.dataset.oldOverflow = el.style.overflow
-
-            el.style.height = el.scrollHeight + 'px'
-            el.style.overflow = 'hidden'
-          },
-
-          leave(el: HTMLDivElement) {
-            if (el.scrollHeight !== 0) {
-              // for safari: add class after set height, or it will jump to zero height suddenly, weired
-              el.classList.add('collapse-transition')
-              // fix #968 collapse animation failure.
-              // in vue3.0.4, transitionProperty is set 'none' to avoid 'v-leave-from' issue
-              el.style.transitionProperty = 'height'
-              el.style.height = '0'
-              el.style.paddingTop = '0'
-              el.style.paddingBottom = '0'
-            }
-          },
-
-          afterLeave(el: HTMLDivElement) {
-            el.classList.remove('collapse-transition')
-            el.style.height = ''
-            el.style.overflow = el.dataset.oldOverflow || '0'
-            el.style.paddingTop = el.dataset.oldPaddingTop || '0'
-            el.style.paddingBottom = el.dataset.oldPaddingBottom || '0'
-          }
-        }
+    enter(el: HTMLDivElement) {
+      el.dataset.oldOverflow = el.style.overflow
+      if (el.scrollHeight !== 0) {
+        el.style.height = el.scrollHeight + 'px'
+        el.style.paddingTop = el.dataset.oldPaddingTop || '0'
+        el.style.paddingBottom = el.dataset.oldPaddingBottom || '0'
+      } else {
+        el.style.height = ''
+        el.style.paddingTop = el.dataset.oldPaddingTop || '0'
+        el.style.paddingBottom = el.dataset.oldPaddingBottom || '0'
       }
+
+      el.style.overflow = 'hidden'
+    },
+
+    afterEnter(el: HTMLDivElement) {
+      // for safari: remove class then reset height is necessary
+      el.classList.remove('collapse-transition')
+      el.style.height = ''
+      el.style.overflow = el.dataset.oldOverflow || '0'
+    },
+
+    beforeLeave(el: HTMLDivElement) {
+      if (!el.dataset) (el.dataset as any) = {}
+      el.dataset.oldPaddingTop = el.style.paddingTop
+      el.dataset.oldPaddingBottom = el.style.paddingBottom
+      el.dataset.oldOverflow = el.style.overflow
+
+      el.style.height = el.scrollHeight + 'px'
+      el.style.overflow = 'hidden'
+    },
+
+    leave(el: HTMLDivElement) {
+      if (el.scrollHeight !== 0) {
+        // for safari: add class after set height, or it will jump to zero height suddenly, weired
+        el.classList.add('collapse-transition')
+        // fix #968 collapse animation failure.
+        // in vue3.0.4, transitionProperty is set 'none' to avoid 'v-leave-from' issue
+        el.style.transitionProperty = 'height'
+        el.style.height = '0'
+        el.style.paddingTop = '0'
+        el.style.paddingBottom = '0'
+      }
+    },
+
+    afterLeave(el: HTMLDivElement) {
+      el.classList.remove('collapse-transition')
+      el.style.height = ''
+      el.style.overflow = el.dataset.oldOverflow || '0'
+      el.style.paddingTop = el.dataset.oldPaddingTop || '0'
+      el.style.paddingBottom = el.dataset.oldPaddingBottom || '0'
     }
-  })
+  }
 </script>
 <style>
   .collapse-transition {
