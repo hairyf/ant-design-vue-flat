cb7f48a2209c33ec9b11a93b46b414b617d461f6
TuiMao233 <wwu710632@gmail.com>
2021-07-15 18:58:35 +0800

    🐛 修复滚动条错误。

--- src/plugins/use-plugins.ts
+++ src/plugins/use-plugins.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-03-19 16:35:43
- * @LastEditTime: 2021-06-23 11:09:44
+ * @LastEditTime: 2021-07-15 18:47:52
  * @Description: 引入插件
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
