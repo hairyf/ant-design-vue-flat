2d07fcf162ad17f8208681c6e8c984ac80a30c77
13169588842 <2531161998@qq.com>
2021-07-15 14:56:03 +0800

    组件新增

---
+++ package/components/input/src/InputSearch.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Author: Mr.wang
+ * @Date: 2021-07-14 09:29:24
+ * @LastEditTime: 2021-07-15 14:47:45
+ * @Description: 
+ * @LastEditors: Mr.wang
+ * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
+-->
+<template>
+  <a-input-search v-bind="props" />
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({ name: 'CalInputSearch' })
+</script>
+<script lang="ts" setup>
+  import { defineProps } from 'vue'
+  import { useTheme } from '../../../utils/theme'
+  const props = defineProps({})
+  useTheme('Input')
+</script>
+<style lang="scss">
+  .ant-input-search {
+    border-radius: 20px !important;
+  }
+  .ant-input-affix-wrapper:hover {
+    border-color: var(--input-hover-color) !important;
+  }
+  .ant-input-affix-wrapper:focus {
+    border-color: var(--input-hover-color);
+    box-shadow: 0 2px 0 0 var(--input-focus-color) !important;
+  }
+  .ant-input-affix-wrapper-focused {
+    border-color: var(--input-hover-color) !important;
+    box-shadow: 0 2px 0 0 var(--input-focus-color) !important;
+  }
+  .ant-input-search-icon {
+    font-size: 15px;
+    color: #8c8c8c;
+  }
+  input {
+    &::placeholder {
+      color: #8c8c8c !important;
+    }
+  }
+</style>
--- package/components/input/src/InputSelect.vue
+++ package/components/input/src/InputSelect.vue
@@ -1,46 +1,126 @@
 <!--
  * @Author: Mr.wang
- * @Date: 2021-07-14 09:29:24
- * @LastEditTime: 2021-07-15 14:33:50
- * @Description: 
+ * @Date: 2021-07-15 14:48:30
+ * @LastEditTime: 2021-07-15 14:55:51
+ * @Description: 选择框
  * @LastEditors: Mr.wang
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
 -->
 <template>
-  <a-input-search v-bind="props" />
+  <a-select
+    class="cal-select"
+    v-model:value="inputValue"
+    dropdownClassName="cal-select-option "
+    v-bind="props"
+    :style="{ width: analyUnit(width) }"
+    allowClear
+  >
+    <slot />
+  </a-select>
 </template>
-<script lang="ts">
-  import { defineComponent } from 'vue'
-  export default defineComponent({ name: 'CalInputSelect' })
-</script>
 <script lang="ts" setup>
-  import { defineProps } from 'vue'
+  import { analyUnit } from '@tuimao/utils'
+  import { computed, defineProps, defineEmits } from 'vue-demi'
   import { useTheme } from '../../../utils/theme'
-  const props = defineProps({})
+  const props = defineProps({
+    modelValue: [String, Number],
+    width: {
+      type: [Number, String],
+      default: 175
+    }
+  })
+  const emit = defineEmits(['update:modelValue'])
+  const inputValue = computed({
+    get: () => (props.modelValue ? props.modelValue : undefined),
+    set(value) {
+      emit('update:modelValue', value)
+    }
+  })
+  useTheme('Common')
   useTheme('Input')
 </script>
 <style lang="scss">
-  .ant-input-search {
-    border-radius: 20px !important;
+  .cal-select {
+    &.ant-select {
+      .ant-select-arrow {
+        transform: rotateZ(0deg);
+        transition: transform 0.2s;
+      }
+    }
+    &.ant-select-open {
+      .ant-select-arrow {
+        transform: rotateZ(180deg);
+      }
+    }
+
+    .ant-select-selection-placeholder {
+      @apply text-gray-500;
+    }
+    .anticon {
+      @apply text-gray-500;
+    }
+
+    // 选择前的样式
+    .ant-select-selector {
+      background: none !important;
+      border-top: none !important;
+      border-left: none !important;
+      border-right: none !important;
+      border-color: #8c8c8c !important;
+      border-radius: 0 !important;
+
+      &:hover {
+        border-color: var(--common-primary-color) !important;
+      }
+    }
+
+    // 选择后的样式
+    &.ant-select-focused {
+      .ant-select-selector {
+        border-color: var(--common-primary-color) !important;
+        box-shadow: 0 2px 0 0 var(--common-primary-color-light-8) !important;
+      }
+    }
   }
-  .ant-input-affix-wrapper:hover {
-    border-color: var(--input-hover-color) !important;
-  }
-  .ant-input-affix-wrapper:focus {
-    border-color: var(--input-hover-color);
-    box-shadow: 0 2px 0 0 var(--input-focus-color) !important;
-  }
-  .ant-input-affix-wrapper-focused {
-    border-color: var(--input-hover-color) !important;
-    box-shadow: 0 2px 0 0 var(--input-focus-color) !important;
-  }
-  .ant-input-search-icon {
-    font-size: 15px;
-    color: #8c8c8c;
-  }
-  input {
-    &::placeholder {
-      color: #8c8c8c !important;
+
+  .cal-select-option {
+    background-color: transparent !important;
+    box-shadow: none !important;
+    padding: 7px 0 0 !important;
+    // .ant-select-item-option {}
+    &.ant-select-dropdown-placement-bottomLeft {
+      overflow: initial !important;
+    }
+    .ant-select-dropdown-content {
+      background-color: #ffffff !important;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.05), 0px 0px 20px rgba(0, 0, 0, 0.02);
+      border-radius: 5px;
+      padding: 8px 0 !important;
+      position: relative;
+      &::before {
+        content: '';
+        width: 0;
+        height: 0;
+        left: 0;
+        right: 0;
+        top: -2.5px;
+        margin: auto;
+        position: absolute;
+        border-radius: 4px;
+        transform: rotateZ(45deg);
+        border: transparent solid 10px;
+        border-top-color: #fff;
+        border-left-color: #fff;
+      }
+    }
+
+    .ant-select-item-option-selected {
+      background-color: #ffffff !important;
+      font-weight: 400 !important;
+    }
+    .ant-select-item-option-active {
+      background-color: var(--common-primary-color) !important;
+      color: var(--input-solid-color) !important;
     }
   }
 </style>
