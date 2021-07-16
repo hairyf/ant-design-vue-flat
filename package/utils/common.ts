6e52f4d2fb33cdf0eda191bc8c58074353f96963
燃芯 <56920162+RanxinStart@users.noreply.github.com>
2021-07-16 22:26:03 +0800

    修改grid

--- package/utils/common.ts
+++ package/utils/common.ts
@@ -1,11 +1,12 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-12 17:51:26
- * @LastEditTime: 2021-07-15 09:41:15
+ * @LastEditTime: 2021-07-16 22:23:01
  * @Description:
  * @LastEditors: Zhilong
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
+import { analyUnit } from '@tuimao/utils'
 import { DefineComponent } from 'vue'
 
 /**
@@ -30,4 +31,28 @@ export const randomString = (size = 5) => {
   stringLength = size < 1 ? 1 : size
   const randomString = Math.random().toString(36)
   return randomString.slice(stringLength * -1, randomString.length)
+}
+
+/**
+ * 将size转换
+ * @param { Number | String | { width:Number|String,height:Number|String } | { w:Number|String,h:Number|String } }
+ * @returns { width , height } 的对象
+ */
+type Key = string | number
+type ToSizeObject = { width: Key; height: Key } | { w: Key; h: Key }
+export type ToSize = Key | ToSizeObject
+export const toSize = (size: ToSize) => {
+  if (typeof size === 'string' || typeof size === 'number') {
+    // 正方形的单条数据
+    return { width: analyUnit(size), height: analyUnit(size) }
+  }
+  if (typeof size === 'object') {
+    // 对象类型的数据
+    return {
+      width: analyUnit((size as any)?.width || (size as any)?.w || '0'),
+      height: analyUnit((size as any)?.height || (size as any)?.h || '0')
+    }
+  }
+  // 默认数据
+  return { width: '', height: '' }
 }
