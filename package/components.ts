d95707aaf7edb30b35008f678826482039a78cf1
TuiMao233 <wwu710632@gmail.com>
2021-07-21 11:15:10 +0800

    ✨ 新增多层分类选择。

--- package/components.ts
+++ package/components.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:12:41
- * @LastEditTime: 2021-07-20 21:45:53
+ * @LastEditTime: 2021-07-21 09:37:30
  * @LastEditTime: 2021-07-12 19:58:42
  * @Description:
  * @LastEditors: Mr.Mao
@@ -17,7 +17,7 @@ export { default as CalModel } from './components/modal/src/Modal'
 export { default as CalEcharts } from './components/echarts/src/Echarts'
 export { default as CalForm } from './components/form/src/Form'
 export { default as CalFormItem } from './components/form/src/FormItem'
-export { default as CalPerfectScrollbar } from './components/perfect-scrollbar/src/PerfectScrollbar.vue'
+// export { default as CalPerfectScrollbar } from './components/perfect-scrollbar/src/PerfectScrollbar.vue'
 export { default as CalEditor } from './components/editor/src/Editor.vue'
 export { default as CalBreadcrumb } from './components/breadcrumb/src/Breadcrumb'
 export { default as CalBreadcrumbItem } from './components/breadcrumb/src/BreadcrumbItem'
--- src/plugins/use-plugins.ts
+++ src/plugins/use-plugins.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-03-19 16:35:43
- * @LastEditTime: 2021-07-20 15:51:05
+ * @LastEditTime: 2021-07-21 09:39:42
  * @Description: 引入插件
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -12,10 +12,10 @@ import store from '@/store'
 import 'animate.css'
 
 // 全量引入 ant-design-vue-flat 组件库
-// import AntdFlat from '~/index'
-// import '~/main.scss'
-import AntdFlat from '../../dist/ant-design-vue-flat.esm.js'
-import '../../dist/ant-design-vue-flat.esm.css'
+import AntdFlat from '~/index'
+import '~/main.scss'
+// import AntdFlat from '../../dist/ant-design-vue-flat.esm.js'
+// import '../../dist/ant-design-vue-flat.esm.css'
 
 // 引入颜色选择框
 import 'vanilla-colorful'
