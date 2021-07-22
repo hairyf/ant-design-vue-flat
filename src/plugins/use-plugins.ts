3cc9ef9619c05b70a14cdfb4108851e04f735954
TuiMao233 <wwu710632@gmail.com>
2021-07-22 15:36:20 +0800

    ✨ 删除不必要文件。

--- src/plugins/use-plugins.ts
+++ src/plugins/use-plugins.ts
@@ -1,14 +1,13 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-03-19 16:35:43
- * @LastEditTime: 2021-07-21 11:27:26
+ * @LastEditTime: 2021-07-22 15:33:35
  * @Description: 引入插件
- * @LastEditors: Pan.Yu.Lin
+ * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
 import { App } from 'vue'
 import router from '@/router'
-import store from '@/store'
 import 'animate.css'
 
 // 全量引入 ant-design-vue-flat 组件库
@@ -28,7 +27,6 @@ import 'vanilla-colorful'
 export const usePlugins = (app: App<Element>) => {
   // 引入插件
   app.use(router)
-  app.use(store)
   app.use(AntdFlat)
   app.use(ElementPlus, { locale })
 }
