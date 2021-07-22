542c49b29cb53282955960287cda1c70eba915e0
webPanyulin <2841113791@qq.com>
2021-07-22 16:04:34 +0800

    📦 tree不影响其他组件样式

--- package/components/input/src/inputTreeSelect.vue
+++ package/components/input/src/inputTreeSelect.vue
@@ -1,12 +1,16 @@
 <!--
  * @Author: Pan.Yu.Lin
  * @Date: 2021-07-21 09:10:04
- * @LastEditTime: 2021-07-21 11:20:37
+ * @LastEditTime: 2021-07-22 16:04:09
  * @Description: 
  * @LastEditors: Pan.Yu.Lin
 -->
 <template>
-  <a-tree-select v-bind="props" :show-checked-strategy="SHOW_PARENT" />
+  <a-tree-select
+    class="cal-custom-tree-select"
+    v-bind="props"
+    :show-checked-strategy="SHOW_PARENT"
+  />
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue'
@@ -21,30 +25,34 @@
   useTheme('Input')
 </script>
 <style lang="scss">
-  .ant-select-focused:not(.ant-select-disabled).ant-select:not(.ant-select-customize-input)
+  .cal-custom-tree-select {
+    &.ant-select:not(.ant-select-customize-input) .ant-select-selector {
+      border-radius: 0 !important;
+    }
+    &.ant-select-focused:not(.ant-select-disabled).ant-select:not(.ant-select-customize-input)
+      .ant-select-selector {
+      box-shadow: none !important;
+    }
     .ant-select-selector {
-    box-shadow: none !important;
-  }
-  .ant-select:not(.ant-select-customize-input) .ant-select-selector {
-    border-radius: 0 !important;
-  }
-  .ant-select-selector {
-    background: none !important;
-    border-top: none !important;
-    border-left: none !important;
-    border-right: none !important;
-    border-color: #8c8c8c !important;
-    border-radius: none !important;
-    color: #001426;
-    &::-webkit-input-placeholder {
-      @apply text-gray-500;
-    }
-    &:focus,
-    &:hover {
-      border-color: var(--input-hover-color) !important;
+      background: none !important;
+      border-top: none !important;
+      border-left: none !important;
+      border-right: none !important;
+      border-color: #8c8c8c !important;
+      border-radius: none !important;
+      color: #001426;
+      &::-webkit-input-placeholder {
+        @apply text-gray-500;
+      }
+      &:focus,
+      &:hover {
+        border-color: var(--input-hover-color) !important;
+      }
     }
   }
-  .ant-select-tree li span.ant-select-tree-switcher, .ant-select-tree li span.ant-select-tree-iconEle{
-    line-height: 16px!important;
+
+  .ant-select-tree li span.ant-select-tree-switcher,
+  .ant-select-tree li span.ant-select-tree-iconEle {
+    line-height: 16px !important;
   }
 </style>
--- src/view/component.vue
+++ src/view/component.vue
@@ -1,12 +1,21 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:29:03
- * @LastEditTime: 2021-07-22 15:32:13
+ * @LastEditTime: 2021-07-22 15:57:58
  * @Description: 
- * @LastEditors: Mr.Mao
+ * @LastEditors: Pan.Yu.Lin
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
 -->
 <template>
+<cal-input-search></cal-input-search>
+<cal-input-tree-select 
+
+    style="width: 300px"
+    v-model:value="treeValue"
+    :tree-data="treeData"
+    tree-checkable
+    allow-clear
+    placeholder="Please select" />
   <cal-input-date></cal-input-date>
   <cal-input-time-picker
     class="w-208"
@@ -234,6 +243,7 @@
   </cal-space>
 </template>
 <script lang="ts" setup>
+  const treeValue = ref<any[]>([])
   const option = ref([
     {
       name: '一级分类选择-1',
@@ -252,6 +262,43 @@
       ]
     }
   ])
+  const treeData = ref([
+  {
+    title: 'Node1',
+    value: '0-0',
+    key: '0-0',
+    children: [
+      {
+        title: 'Child Node1',
+        value: '0-0-0',
+        key: '0-0-0',
+      },
+    ],
+  },
+  {
+    title: 'Node2',
+    value: '0-1',
+    key: '0-1',
+    children: [
+      {
+        title: 'Child Node3',
+        value: '0-1-0',
+        key: '0-1-0',
+        disabled: true,
+      },
+      {
+        title: 'Child Node4',
+        value: '0-1-1',
+        key: '0-1-1',
+      },
+      {
+        title: 'Child Node5',
+        value: '0-1-2',
+        key: '0-1-2',
+      },
+    ],
+  },
+]);
   const select = ref([])
   import { ref, watchEffect } from 'vue'
   import { CalModel } from '~/components'
