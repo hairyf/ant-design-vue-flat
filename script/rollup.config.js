770aacacadf6b21a4ff2caf3db1925cc2820754e
TuiMao233 <wwu710632@gmail.com>
2021-07-25 12:12:26 +0800

    ✨ 更新组件库。

--- script/rollup.config.js
+++ script/rollup.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-07-07 15:17:55
- * @LastEditTime: 2021-07-20 14:44:55
+ * @LastEditTime: 2021-07-25 12:10:55
  * @Description:
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -71,6 +71,7 @@ export default defineConfig({
     'echarts',
     'vue-echarts',
     'nprogress',
-    /@simonwep\/pickr/
+    /@simonwep\/pickr/,
+    '@tuimao/utils'
   ]
 })
