da09d43955b56557bcf7f6ac391f50576b95e247
webPanyulin <2841113791@qq.com>
2021-07-29 15:16:19 +0800

    📦 修改

--- package/components/radio/src/Radio.vue
+++ package/components/radio/src/Radio.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.wang
  * @Date: 2021-07-12 19:25:11
- * @LastEditTime: 2021-07-29 15:14:01
+ * @LastEditTime: 2021-07-29 15:16:07
  * @Description: 单选
  * @LastEditors: Pan.Yu.Lin
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -23,8 +23,8 @@
   import { useTheme } from '../../../utils/theme'
   import { Radio as ARadio } from 'ant-design-vue'
   const props = defineProps({
-    value: String || Number,
-    label: String || Number
+    value: String || Number || undefined,
+    label: String || Number || undefined
   })
   useTheme('Common')
 </script>
