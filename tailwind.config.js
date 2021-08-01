89f00eca25d202e3af954c80e6bfc22bbd22eccd
TuiMao233 <wwu710632@gmail.com>
2021-08-01 10:32:48 +0800

    ✨ 更新内容。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-05-26 14:15:49
- * @LastEditTime: 2021-08-01 10:09:53
+ * @LastEditTime: 2021-08-01 10:30:20
  * @Description:
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -13,9 +13,7 @@ const presets = getPresets({
   theme: {
     colors: {
       ...colors,
-      gray: {
-        450: '#8c8c8c'
-      }
+      gray: { 450: '#8c8c8c' }
     }
   },
   important: false
