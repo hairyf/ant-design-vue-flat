572796dd527c6b3f942a4d48514bae99f1624c5a
TuiMao233 <wwu710632@gmail.com>
2021-08-04 14:02:52 +0800

    📦 新增同步命令。

---
+++ scripts/subtree.pushs.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: Mr.Mao
+ * @Date: 2021-08-04 11:55:41
+ * @LastEditTime: 2021-08-04 13:58:08
+ * @Description: 
+ * @LastEditors: Mr.Mao
+ * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
+ */
+import { subtrees } from './subtree.config'
+import { execSync } from 'child_process'
+
+for (const [prefix, repository] of subtrees) {
+  execSync(`git subtree push --prefix=${prefix} ${repository} master --squash`)
+}
