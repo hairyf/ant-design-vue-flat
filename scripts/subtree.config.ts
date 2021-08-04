14da12be75fadfa48762f6b4c954dd07cd84b60c
TuiMao233 <wwu710632@gmail.com>
2021-08-04 13:57:26 +0800

    📦 新增命令。

--- scripts/subtree.config.ts
+++ scripts/subtree.config.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-08-04 11:31:35
- * @LastEditTime: 2021-08-04 11:34:40
+ * @LastEditTime: 2021-08-04 12:02:36
  * @Description: 
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
  */
-const packages = [
+export const subtrees = [
   ["packages/ant-design-vue-flat", "https://github.com/TuiMao233/ant-design-vue-flat.git"],
   ["packages/front-end-docs", "git@159.75.17.184:root/front-end-docs.git"],
   ["packages/orange-internet-mall-admin", "git@159.75.17.184:root/orange-internet-mall-admin.git"],
