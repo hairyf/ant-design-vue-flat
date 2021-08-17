84d11e57bb28397e0fd4cda552632f983e4a42ff
webPanyulin <2841113791@qq.com>
2021-08-17 09:16:16 +0800

    📦 提交

--- src/view/component.vue
+++ src/view/component.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Mr.Mao
  * @Date: 2021-07-08 15:29:03
- * @LastEditTime: 2021-08-17 09:04:32
+ * @LastEditTime: 2021-08-17 09:16:03
  * @Description: 
  * @LastEditors: Pan.Yu.Lin
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -66,12 +66,6 @@
   </cal-input-time-picker>
 </template>
 <script lang="ts" setup>
- const appointParameter = ref({
-    children: 'children',
-    title: 'name',
-    key: 'id',
-    value: 'id'
-  })
   const formState = ref({
     businessHours: []
   })
